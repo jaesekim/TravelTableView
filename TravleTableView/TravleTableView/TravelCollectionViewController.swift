fddefc52f543981aae9190dd7ad61360bb8cdff6
jaesekim <jaeseki@gmail.com>
2024-01-09 07:12:16 +0900

    파일 재업로드

---
+++ TravleTableView/TravleTableView/TravelCollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TravelCollectionViewController.swift
+//  TravleTableView
+//
+//  Created by 김재석 on 1/9/24.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class TravelCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
---
+++ TravleTableView/TravleTableView/TravelTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTableViewCell.swift
+//  TravleTableView
+//
+//  Created by 김재석 on 1/9/24.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    @IBOutlet var travelImageView: UIImageView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
